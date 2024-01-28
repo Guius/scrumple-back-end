6d7e6a6f2a22c4cf6486b57db89019442ae0b94e
Guillaume Vitry <guy.vitry@aqua-superpower.com>
2024-01-28 19:49:07 +0000

    removing deprecated sprint methods

--- src/services/sprints/sprint.service.ts
+++ src/services/sprints/sprint.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Sprint } from '../../entities/sprint.entity';
 import { DataSource, IsNull, Repository } from 'typeorm';
@@ -38,56 +38,4 @@ export class SprintService {
       },
     });
   }
-
-  /**
-   * Creates a new sprint if there are no incomplete sprints.
-   * @returns {Promise<Sprint>} A promise that resolves with the newly created Sprint entity.
-   * @throws {BadRequestException} If there is an incomplete sprint.
-   */
-  async createSprintOld(): Promise<Sprint> {
-    // 1. Check if there is a sprint that is not complete
-    const [notCompleteSprint, sprintCount] =
-      await this.sprintRepository.findAndCount({
-        // where: {
-        //   complete: IsNull(),
-        // },
-      });
-
-    if (notCompleteSprint.length > 0) {
-      Logger.warn(
-        `SprintService | createSprint - Client is trying to create new sprint when one is still not complete`,
-      );
-      throw new BadRequestException(
-        `SprintService | createSprint - Client is trying to create new sprint when one is still not complete`,
-      );
-    }
-
-    // 2. All is good, create the new sprint
-    // generate the sprint number
-    const sprintNumber: number = sprintCount + 1;
-
-    // create the sprint
-    const sprintToSave: Sprint = await this.sprintRepository.create({
-      sprint_number: sprintNumber,
-    });
-
-    // save the sprint
-    return await this.sprintRepository.save(sprintToSave);
-  }
-
-  /**
-   * Retrieves the current sprint, i.e., the sprint that is not complete
-   *
-   * @returns {Promise<Sprint>} Promise that resolves to the current sprint.
-   */
-  // async getCurrentSprint(): Promise<Sprint> {
-  //   return await this.sprintRepository.findOne({
-  //     order: {
-  //       sprint_number: 'DESC', // Order by sprint number in descending order to get the highest number first. This is in the odd case that there would be 2 current sprints, we want to returns the latest one
-  //     },
-  //     where: {
-  //       complete: IsNull(), // Only retrieve sprints that are not complete.
-  //     },
-  //   });
-  // }
 }
